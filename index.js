221f21c20035c1e38a1ce7aab677fefacafcaad5
Isaac Reji <isaac.reji@kalvium.community>
2024-12-10 14:43:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const { resolve } = require('path');
-
+require("dotenv").config({
+  path:"./.env",
+})
+isAdmin=process.env.IS_ADMIN
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (isAdmin=="true"){
+    res.send({"isAdmin":isAdmin,"message":"Welcome Admin"})
+  }
+  else if(isAdmin=="false"){
+    res.send({"isAdmin":isAdmin,"message":"Welcome User"})
+  }
 });
 
 app.listen(port, () => {
